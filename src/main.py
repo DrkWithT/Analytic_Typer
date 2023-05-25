3e25d6f944a74c71d3b777d8743684dc5424d5b6
drkwitht <derektan3510@gmail.com>
2023-05-24 19:57:15 -0700

    Added server and dummy wordgen.

--- src/main.py
+++ src/main.py
@@ -2,21 +2,49 @@
     main.py
     @brief Runs the main server code.
     @author Derek Tan & ??
+    @version 0.1.0
 """
 
-from flask import Flask, render_template
 from atexit import register
+from flask import Flask, render_template, request
+from utils.testmaker import generate_prose
 
 def exit_handler():
-    # todo: close database connections, etc.
+    # TODO: close sqlite connections, etc.
     print('Stopped app server.')
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/typer')
+@app.route('/', methods=['GET'])
+@app.route('/typer', methods= ['GET'])
 def serve_typer_page():
     return render_template('typer.html', page_name='Type Test')
+
+@app.route('/api/typer', methods=['GET'])
+def api_get():
+    raw_words = generate_prose(True, None, None)
+
+    # build dictionary to store automatic JSON formatted data... this is for setting the user's type test! 
+    response_data = {
+        "original": raw_words
+    }
+
+    return response_data
+
+@app.route('/api/typer', methods=['POST'])
+def api_post_result():
+    typerResults = None
+    ok_status = True  # NOTE: if the JSON from the type tester client was ok... this may need a property check later: user and original text must be present.
+    
+    try:
+        typerResults = request.get_json()
+    except Exception as err:
+        print(f'JSON error: {err}')
+        ok_status = False
+    else:
+        print(f'Original Text: {typerResults.original}\nUser Text: {typerResults.user}'); # TODO: replace this print statment with function calls for the analytical code.
+
+    return {"ok": ok_status}
 
 @app.errorhandler(Exception)
 def handle_server_error(error: Exception):
